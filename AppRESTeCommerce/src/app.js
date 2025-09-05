77e07398b40e8b08ee1f63838ab1455466ad643d
Arath90 <ar90we17@gmail.com>
2025-09-04 19:38:07 -0700

    Más comentarios nota 5.1 DSW

--- AppRESTeCommerce/src/app.js
+++ AppRESTeCommerce/src/app.js
@@ -6,20 +6,25 @@ import config from './config/config';
 //luego se exporta la app para que pueda ser usada en otros archivos del proyecto.
 const app = express();
 // estos settings son para que heroku pueda asignar el puerto dinamicamente
-app.set('port', config.PORT);
-// Middlewares generales
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.set('port', config.PORT); //se establece el puerto en el que se ejecutara el servidor, desde config.js
+// Middlewares generales-------------------------------------------------------------------------
+app.use(cors());//cors es un middleware que permite el acceso a la api desde cualquier origen (dominio) y con cualquier metodo (GET, POST, PUT, DELETE, etc)
+
+app.use(morgan('dev'));//morgan es un middleware que muestra en consola las peticiones que llegan al servidor, asi como su metodo y su estado (200, 404, 500, etc).
+
+app.use(express.json());//express.json es un middleware que permite recibir datos en formato json en el body de las peticiones
+
+app.use(express.urlencoded({ extended: false }));//express.urlencoded es un middleware que permite recibir datos en formato urlencoded en el body de las peticiones
+//configuracion de las rutas de la aplicacion--------------------------------------------------
 //rutas para ver que el servidor funciona
-const api = config.API_URL;
-app.get(`${api}`, (req,res)=>{
+const api = config.API_URL; //ruta base de la api desde config.js 
+app.get(`${api}`, (req,res)=>{//que es este monton de simbolos? es una template string, permite insertar variables en un string usando ${variable}
     res.send(
         `<h1>RESTful running in root (página base)</h1> <p> eCommerce: <b>${api}/api-docs</b> for more information.</p>`
     );
 })
-//ruta de prueba para comprobar que el servidor funciona en otro path
+//ruta de prueba para comp
+// robar que el servidor funciona en otro path
 app.get('/DrFIC', (req,res)=>{
     //send es un metodo de express para enviar una respuesta al cliente
     //en este caso se envia un html simple con un enlace a la documentacion de la api
@@ -27,9 +32,11 @@ app.get('/DrFIC', (req,res)=>{
     res.send(
         `<h1>RESTful running in DrFIC (como comprobacion y prueba)</h1> <p> eCommerce: <b>${api}/api-docs</b> for more information.</p>`
     );
+    //express es el nucleo de la  API, y morgan es un middleware que permite ver en consola las peticiones que llegan al servidor
+    //cors es otro middleware que permite el acceso a la api desde cualquier origen (dominio) y con cualquier metodo (GET, POST, PUT, DELETE, etc)
+    //express.json es un middleware que permite recibir datos en formato json en el body de las peticiones
+    //express.urlencoded es un middleware que permite recibir datos en formato urlencoded en el body de las peticiones
+
 })
-// Routes
-// Swagger Docs
-// Middleware para el manejo de errores
-// Export App
+//exportacion de la app para que pueda ser usada en otros archivos del proyecto
 export default app;
--- AppRESTeCommerce/src/index.js
+++ AppRESTeCommerce/src/index.js
@@ -1,6 +1,16 @@
-import app from './app';
-import config from './config/config';
-app.listen(app.get('port'));
+//index.js establece el puerto en el que se ejecutara el servidor y arranca la app.
+//luego muestra en consola la url en la que se esta ejecutando el servidor.
+//se importa la app desde el archivo app.js y la configuracion desde config.js
+
+
+import app from './app';//importa la app desde app.js
+import config from './config/config';//importa la configuracion desde config.js
+app.listen(app.get('port'));//arranca la app en el puerto establecido en app.js pero como?
+//  pues usando el metodo listen de express, que recibe como parametro el puerto en el que se ejecutara el servidor 
+// aun asi  no se ve claro, porque app.get('port')?
+//  pues porque en app.js se establece el puerto usando app.set('port', config.PORT);
+//  y config.PORT se lee desde las variables de entorno en config.js
+// basicamente index.js-->app.js-->config.js-->.env 
 console.log(
   `Server is running on: http://${config.HOST}:${app.get('port')}${
     config.API_URL
